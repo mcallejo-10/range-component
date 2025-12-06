9eb7eda359faf5ddd030d95b393b3e9ff4715d33
Miranda <mirandacallejonh@gmail.com>
2025-12-06 14:57:49 +0100

    feat: add mock API services for range data

---
+++ src/services/rangeService.ts
@@ -0,0 +1,23 @@
+import { NormalRangeResponse, FixedRangeResponse } from "@/types/range.types";
+
+const simulateNetworkDelay = () => {
+    const delay = Math.random() * 500 + 300; 
+    return new Promise(resolve => setTimeout(resolve, delay));
+};
+
+// Exercice 1: Normal Range Service
+export const fetchNormalRangeValues = async (): Promise<NormalRangeResponse> => {
+    await simulateNetworkDelay();
+    return {
+        min: 1,
+        max: 100,
+    };
+}
+
+// Exercice 2: Fixed Range Service
+export const fetchFixedRangeValues = async (): Promise<FixedRangeResponse> => {
+    await simulateNetworkDelay();
+    return {
+        rangeValues: [1.99, 5.99, 10.99, 30.99, 50.99, 70.99]
+    };
+};
